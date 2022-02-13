7b0e3d4f344e9ceb868d5d00f1f2f453e5c1ef18
ViktorVx <victorptrv@yandex.ru>
2022-02-13 18:59:19 +0300

    Add table from test storage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 "use strict";
+import {PERSONS} from './storage.js';
+
+function ready() {
+    let personsTable = document.getElementById("personsTable");
+    for (let person of PERSONS) {
+        console.log(person);
+        let tr = document.createElement('tr');
+
+        let tdLastName = document.createElement('td');
+        tdLastName.innerText = person.lastName;
+        tr.append(tdLastName);
+
+        let tdName = document.createElement('td');
+        tdName.innerText = person.firstName;
+        tr.append(tdName);
+
+        let tdMiddleName = document.createElement('td');
+        tdMiddleName.innerText = person.middleName;
+        tr.append(tdMiddleName);
+
+        personsTable.append(tr);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
---
+++ js/storage.js
@@ -0,0 +1,15 @@
+"use strict";
+export const PERSONS = [
+    {firstName: "name1", lastName:"ln1", middleName:"mn1"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name2", lastName:"ln2", middleName:"mn2"},
+    {firstName: "name3", lastName:"ln3", middleName:"mn3"}
+];
